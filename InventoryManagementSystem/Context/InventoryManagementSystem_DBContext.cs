e5bd5929bf155167959ef38ca1cc6475ee9e6e58
Sirajul-rana <sirajul_rana@outlook.com>
2017-02-25 01:31:05 +0600

    Login page complete

---
+++ InventoryManagementSystem/Context/InventoryManagementSystem_DBContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using InventoryManagementSystem.Models;
+
+namespace InventoryManagementSystem.Context
+{
+    public class InventoryManagementSystem_DBContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Delivery> Deliveries { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Purchase> Purcheses { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Size> Sizes { get; set; }
+        public DbSet<Sub_Category> SubCategories { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
--- InventoryManagementSystem/Controllers/HomeController.cs
+++ InventoryManagementSystem/Controllers/HomeController.cs
@@ -3,24 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using InventoryManagementSystem.Context;
 using InventoryManagementSystem.Models;
 
 namespace InventoryManagementSystem.Controllers
 {
     public class HomeController : Controller
     {
+        InventoryManagementSystem_DBContext Context = new InventoryManagementSystem_DBContext();
         // GET: Home
-        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(Context.Users.ToList());
+        }
         public ActionResult Login()
         {
             return View();
         }
 
-
-        [HttpPost]
-        public ActionResult Login(User user)
+        public JsonResult CheckLogin(string txt_username, string txt_password)
         {
-            return View(user);
+            
+            var loginresult = Context.Users.FirstOrDefault(u => u.User_username == txt_username && u.User_password == txt_password);
+            if (loginresult != null)
+            {
+                return Json(loginresult.User_username, JsonRequestBehavior.AllowGet);
+            }
+            return Json("InvalidData", JsonRequestBehavior.AllowGet);
         }
     }
 }
